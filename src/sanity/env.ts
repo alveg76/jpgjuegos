82ebe220acfae59fed5d63a4025a25f70be4556a
alveg76 <abcagg76@gmail.com>
2026-01-07 09:53:13 -0500

    Fix Sanity project ID fallback

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -2,7 +2,7 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-01-06'
 
 export const projectId =
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'lrtz32ba'
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'lrt32ba'
 
 export const dataset =
   process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
