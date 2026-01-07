bcea6755adb2924711461d0ec69b4c79ec922bb9
alveg76 <abcagg76@gmail.com>
2026-01-07 10:11:32 -0500

    Set correct Sanity project fallback

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -2,7 +2,7 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION ?? '2025-01-01'
 
 export const projectId =
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? 'lrt32ba'
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? 'lrtz32ba'
 
 export const dataset =
   process.env.NEXT_PUBLIC_SANITY_DATASET ?? 'production'
